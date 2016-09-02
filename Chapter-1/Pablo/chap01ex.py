a67f94b2a0be3156e1255b732d0381ff0191dc39
Pablo Castillo <caztillo@gmail.com>
2016-09-01 19:19:07 -0500

    segundo ejercicio

--- Chapter-1/Pablo/chap01ex.py
+++ Chapter-1/Pablo/chap01ex.py
@@ -9,17 +9,25 @@ from __future__ import print_function
 
 import numpy as np
 import sys
+import pandas as pd
 
-import nsfg
-import thinkstats2
+#import nsfg
+#import thinkstats2
 
+def read_gzip_file(filename):
+	df = pd.read_csv(filename, compression='gzip', header=0, sep=',', quotechar='"')
+	return df
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+    df = read_gzip_file('../../data/2002FemResp.dat.gz')
+
+    print(df.columns)
+
+
 
 
 if __name__ == '__main__':
